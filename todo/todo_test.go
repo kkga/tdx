d4cfc26b80584112db145fcde19c8ff312b57b07
Gadzhi Kharkharov <me@kkga.me>
2021-09-20 15:41:14 +0300

    add more tests

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -26,6 +26,33 @@ func TestInit(t *testing.T) {
 				Due:     time.Date(2007, time.May, 01, 0, 0, 0, 0, time.UTC),
 			},
 		},
+		{
+			"test_data/e0a2e171-8314-4a36-8236-6851b05ff95b.ics",
+			ToDo{
+				Status:  ToDoStatusNeedsAction,
+				Summary: "update sway timer to use timedown instead of swayidle #Later",
+				UID:     "1a0a65abf057464db48d0a9183adb5db@void",
+			},
+		},
+		{
+			"test_data/48949a4f8b8149ec97f5a2715ac7c86b@void.ics",
+			ToDo{
+				Status:  ToDoStatusNeedsAction,
+				Summary: "https://dave.cheney.net/practical-go/",
+				UID:     "48949a4f8b8149ec97f5a2715ac7c86b@void",
+				Due:     time.Date(2021, time.September, 21, 18, 06, 24, 0, time.UTC),
+			},
+		},
+		{
+			"test_data/20070514T103211Z-123404@example.com.ics",
+			ToDo{
+				Status:   ToDoStatusCompleted,
+				Priority: ToDoPriorityHigh,
+				Summary:  "Submit Revised Internet-Draft",
+				UID:      "20070514T103211Z-123404@example.com",
+				Due:      time.Date(2007, time.July, 9, 13, 0, 0, 0, time.UTC),
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -49,7 +76,6 @@ func TestInit(t *testing.T) {
 						vtodo = *comp
 					}
 				}
-
 			}
 
 			got := NewToDo()
