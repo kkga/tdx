9954a26f42e135f05b310a7f472bbd3761c0144b
Gadzhi Kharkharov <me@kkga.me>
2021-09-22 16:38:50 +0300

    upd

--- vdir/vdir.go
+++ vdir/vdir.go
@@ -84,73 +84,71 @@ func NewCollection(path string, name string) (*Collection, error) {
 		Name: name,
 	}
 
-	err := filepath.WalkDir(path, func(pp string, dd fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if dd.Name() == MetaDisplayName {
-			name, err := os.ReadFile(pp)
-			if err != nil {
-				return err
+	// parse dir for metadata
+	err := filepath.WalkDir(
+		path,
+		func(pp string, dd fs.DirEntry, err error) error {
+			if dd.Name() == MetaDisplayName {
+				name, err := os.ReadFile(pp)
+				if err != nil {
+					return err
+				}
+				c.Name = string(name)
 			}
-			c.Name = string(name)
-		}
-		if dd.Name() == MetaColor {
-			color, err := os.ReadFile(pp)
-			if err != nil {
-				return err
+			if dd.Name() == MetaColor {
+				color, err := os.ReadFile(pp)
+				if err != nil {
+					return err
+				}
+				c.Color = string(color)
 			}
-			c.Color = string(color)
-		}
-		return nil
-	})
-
+			return nil
+		},
+	)
 	if err != nil {
 		return nil, err
 	}
-
 	return c, nil
 }
 
 // Collections returns a map of all collections and items in vdir, items have unique id values
 func (v VdirRoot) Collections() (items map[*Collection][]*Item, err error) {
 	items = make(map[*Collection][]*Item)
+	id := 0
 
 	isIcal := func(path string, de fs.DirEntry) bool {
 		return !de.IsDir() && strings.TrimPrefix(filepath.Ext(path), ".") == ical.Extension
 	}
 
-	id := 0
-	err = filepath.WalkDir(v.Path, func(p string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() && p != v.Path {
-			var c, err = NewCollection(p, d.Name())
-			if err != nil {
-				return err
-			}
-
-			err = filepath.WalkDir(c.Path, func(pp string, dd fs.DirEntry, err error) error {
-				if isIcal(pp, dd) {
-
-					item, err := NewItem(pp)
-					if err != nil {
-						return err
-					}
-					id++
-					item.id = id
-					items[c] = append(items[c], item)
+	// parse dir for vdir collections (folders)
+	err = filepath.WalkDir(
+		v.Path,
+		func(p string, d fs.DirEntry, err error) error {
+			if d.IsDir() && p != v.Path {
+				var c, err = NewCollection(p, d.Name())
+				if err != nil {
+					return err
 				}
-				return nil
-			})
-			if err != nil {
-				return err
+				// parse collection folder for ical files
+				err = filepath.WalkDir(c.Path, func(pp string, dd fs.DirEntry, err error) error {
+					if isIcal(pp, dd) {
+						item, err := NewItem(pp)
+						if err != nil {
+							return err
+						}
+						id++
+						item.id = id
+						items[c] = append(items[c], item)
+					}
+					return nil
+				})
+				if err != nil {
+					return err
+				}
 			}
-
-		}
-		return nil
-	})
+			return nil
+		},
+	)
 	if err != nil {
 		return
 	}
