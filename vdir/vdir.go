266dba8036bd6a048aa10d0df47c2df1d801a5e5
Gadzhi Kharkharov <me@kkga.me>
2021-09-22 16:31:07 +0300

    upd

--- vdir/vdir.go
+++ vdir/vdir.go
@@ -11,17 +11,6 @@ import (
 	"github.com/emersion/go-ical"
 )
 
-func NewVdirRoot(path string) (*VdirRoot, error) {
-	f, err := os.Stat(path)
-	if errors.Is(err, fs.ErrNotExist) {
-		return nil, errors.New("Specified vdir path does not exist.")
-	}
-	if !f.IsDir() {
-		return nil, errors.New("Specified vdir path is not a directory.")
-	}
-	return &VdirRoot{path}, nil
-}
-
 // VdirRoot represents the topmost vdir root folder
 type VdirRoot struct {
 	Path string
@@ -34,96 +23,131 @@ type Collection struct {
 	Path  string
 }
 
+// Item represents an iCalendar item with a unique id
+type Item struct {
+	id   int
+	ical *ical.Calendar
+}
+
 const (
 	MetaDisplayName = "displayname" // MetaDisplayName is a filename vdir uses for collection name
 	MetaColor       = "color"       // MetaColor is a filename vdir uses for collection color
 )
 
-// Collections returns a slice of all vdir collections in root path recursively
-func (v VdirRoot) Collections() (collections []*Collection, err error) {
+// NewVdirRoot initializes a VdirRoot checking that the directory exists
+func NewVdirRoot(path string) (*VdirRoot, error) {
+	f, err := os.Stat(path)
+	if errors.Is(err, fs.ErrNotExist) {
+		return nil, errors.New("Specified vdir path does not exist.")
+	}
+	if !f.IsDir() {
+		return nil, errors.New("Specified vdir path is not a directory.")
+	}
+	return &VdirRoot{path}, nil
+}
+
+// NewItem initializes an Item with a decoded ical from path
+func NewItem(path string) (*Item, error) {
+	i := &Item{}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	dec := ical.NewDecoder(file)
+
+	for {
+		item, err := dec.Decode()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		// filter only items that contain vtodo
+		for _, comp := range item.Children {
+			if comp.Name == ical.CompToDo {
+				i.ical = item
+				break
+			}
+		}
+	}
+	return i, nil
+}
+
+// NewCollection initializes a Collection with a path, name and color parsed from path
+func NewCollection(path string, name string) (*Collection, error) {
+	c := &Collection{
+		Path: path,
+		Name: name,
+	}
+
+	err := filepath.WalkDir(path, func(pp string, dd fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if dd.Name() == MetaDisplayName {
+			name, err := os.ReadFile(pp)
+			if err != nil {
+				return err
+			}
+			c.Name = string(name)
+		}
+		if dd.Name() == MetaColor {
+			color, err := os.ReadFile(pp)
+			if err != nil {
+				return err
+			}
+			c.Color = string(color)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Collections returns a map of all collections and items in vdir, items have unique id values
+func (v VdirRoot) Collections() (items map[*Collection][]*Item, err error) {
+	items = make(map[*Collection][]*Item)
+
+	isIcal := func(path string, de fs.DirEntry) bool {
+		return !de.IsDir() && strings.TrimPrefix(filepath.Ext(path), ".") == ical.Extension
+	}
+
+	id := 0
 	err = filepath.WalkDir(v.Path, func(p string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-
 		if d.IsDir() && p != v.Path {
-			var c = &Collection{}
-			c.Path = p
-			c.Name = d.Name()
+			var c, err = NewCollection(p, d.Name())
+			if err != nil {
+				return err
+			}
 
-			err = filepath.WalkDir(p, func(pp string, dd fs.DirEntry, err error) error {
-				if err != nil {
-					return err
-				}
-				if dd.Name() == MetaDisplayName {
-					name, err := os.ReadFile(pp)
+			err = filepath.WalkDir(c.Path, func(pp string, dd fs.DirEntry, err error) error {
+				if isIcal(pp, dd) {
+
+					item, err := NewItem(pp)
 					if err != nil {
 						return err
 					}
-					c.Name = string(name)
-				}
-				if dd.Name() == MetaColor {
-					color, err := os.ReadFile(pp)
-					if err != nil {
-						return err
-					}
-					c.Color = string(color)
+					id++
+					item.id = id
+					items[c] = append(items[c], item)
 				}
 				return nil
 			})
 			if err != nil {
 				return err
 			}
-			collections = append(collections, c)
-		}
-		return nil
-	})
-	if err != nil {
-		return
-	}
-	return collections, nil
-}
 
-// Items returns a slice of decoded iCalendar items in collection
-func (c Collection) Items() (items map[int]*ical.Calendar, err error) {
-	items = make(map[int]*ical.Calendar)
-
-	isIcal := func(path string, de fs.DirEntry) bool {
-		return !de.IsDir() && strings.TrimPrefix(filepath.Ext(path), ".") == ical.Extension
-	}
-
-	id := 1
-
-	err = filepath.WalkDir(c.Path, func(p string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if isIcal(p, d) {
-			id++
-			file, err := os.Open(p)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-
-			dec := ical.NewDecoder(file)
-
-			for {
-				item, err := dec.Decode()
-				if err == io.EOF {
-					break
-				} else if err != nil {
-					return err
-				}
-
-				// filter only items that contain vtodo
-				for _, comp := range item.Children {
-					if comp.Name == ical.CompToDo {
-						items[id] = item
-						// items = append(items, item)
-					}
-				}
-			}
 		}
 		return nil
 	})
--- vdir/vdir_test.go
+++ vdir/vdir_test.go
@@ -26,32 +26,13 @@ func TestCollections(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			collections, _ := vd.Collections()
-			fmt.Printf("%v\n", collections)
-		})
-	}
-}
-
-func TestItems(t *testing.T) {
-	var tests = []struct {
-		path string
-	}{
-		{testVdirPath},
-	}
-	for _, tt := range tests {
-		t.Run("", func(t *testing.T) {
-			vd, err := NewVdirRoot(tt.path)
+			collections, err := vd.Collections()
 			if err != nil {
 				t.Fatal(err)
 			}
-			collections, _ := vd.Collections()
-			for _, c := range collections {
-				items, err := c.Items()
-				if err != nil {
-					t.Fatal(err)
-				}
-				for id, item := range items {
-					fmt.Printf("%d: %+v\n", id, item.Children[0].Props.Get(ical.PropSummary))
+			for col := range collections {
+				for _, item := range collections[col] {
+					fmt.Printf("%s: %d,  %+v\n", col, item.id, item.ical.Children[0].Props.Get(ical.PropSummary))
 				}
 			}
 		})
