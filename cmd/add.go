28f4ed1b7b17cf54f4bd171d5630c8b08b73a5a1
Gadzhi Kharkharov <me@kkga.me>
2021-09-28 15:37:45 +0300

    parse priority from summary

--- cmd/add.go
+++ cmd/add.go
@@ -17,21 +17,32 @@ import (
 func NewAddCmd() *AddCmd {
 	c := &AddCmd{Cmd: Cmd{
 		// TODO add long description of due date parsing
-		fs:           flag.NewFlagSet("add", flag.ExitOnError),
-		alias:        []string{"a"},
-		shortDesc:    "Add todo",
+		fs:    flag.NewFlagSet("add", flag.ExitOnError),
+		alias: []string{"a"},
+		short: "Add new todo",
+		long: `DUE DATE
+  If todo text contains a date in one of the following
+  formats, it will be applied as due date:
+  * "today", "tomorrow", "in 3 days", "in 2 weeks"
+  * "next week", "next month", "next monday"
+  * ordinal date: "december 1st", "15th november"
+
+PRIORITY
+  If todo text contains one or more "!" chars,
+  they will be converted to priority:
+  * "!!!" (high)
+  * "!!"  (medium)
+  * "!"   (low)`,
 		usageLine:    "[options] <todo>",
 		listRequired: true,
 	}}
 	c.fs.StringVar(&c.listFlag, "l", "", "`list` for new todo")
-	c.fs.StringVar(&c.priority, "p", "", "`priority`: !!!, !!, !")
 	c.fs.StringVar(&c.description, "d", "", "`description` text")
 	return c
 }
 
 type AddCmd struct {
 	Cmd
-	priority    string
 	description string
 }
 
@@ -42,47 +53,43 @@ func (c *AddCmd) Run() error {
 		return errors.New("Provide a todo text")
 	}
 
+	cal := ical.NewCalendar()
+	t := ical.NewComponent(ical.CompToDo)
+	uid := vdir.GenerateUID()
+	t.Props.SetText(ical.PropStatus, string(vdir.StatusNeedsAction))
+	t.Props.SetText(ical.PropUID, uid)
+	cal.Children = append(cal.Children, t)
+
 	summary := strings.Join(args, " ")
 
-	t := ical.NewComponent("VTODO")
-	t.Props.SetText(ical.PropSummary, summary)
-	t.Props.SetText(ical.PropStatus, string(vdir.StatusNeedsAction))
+	if strings.Contains(summary, "!!!") {
+		summary = strings.Trim(strings.Replace(summary, "!!!", "", 1), " ")
+		prioProp := ical.NewProp(ical.PropPriority)
+		prioProp.Value = fmt.Sprint(vdir.PriorityHigh)
+		t.Props.Add(prioProp)
+	} else if strings.Contains(summary, "!!") {
+		summary = strings.Trim(strings.Replace(summary, "!!", "", 1), " ")
+		prioProp := ical.NewProp(ical.PropPriority)
+		prioProp.Value = fmt.Sprint(vdir.PriorityMedium)
+		t.Props.Add(prioProp)
+	} else if strings.Contains(summary, "!") {
+		summary = strings.Trim(strings.Replace(summary, "!", "", 1), " ")
+		prioProp := ical.NewProp(ical.PropPriority)
+		prioProp.Value = fmt.Sprint(vdir.PriorityLow)
+		t.Props.Add(prioProp)
+	}
 
-	uid := vdir.GenerateUID()
-	t.Props.SetText(ical.PropUID, uid)
+	t.Props.SetText(ical.PropSummary, summary)
+
+	if c.description != "" {
+		t.Props.SetText(ical.PropDescription, c.description)
+	}
 
 	now := time.Now()
 	due, _ := naturaldate.Parse(summary, now, naturaldate.WithDirection(naturaldate.Future))
 	if due != now {
 		t.Props.SetDateTime(ical.PropDue, due)
 	}
-
-	// TODO add default due date from conf
-
-	if c.description != "" {
-		t.Props.SetText(ical.PropDescription, c.description)
-	}
-
-	if c.priority != "" {
-		prioProp := ical.NewProp(ical.PropPriority)
-		prioProp.SetValueType(ical.ValueInt)
-		switch c.priority {
-		case "!!!":
-			prioProp.Value = fmt.Sprint(vdir.PriorityHigh)
-			t.Props.Add(prioProp)
-		case "!!":
-			prioProp.Value = fmt.Sprint(vdir.PriorityMedium)
-			t.Props.Add(prioProp)
-		case "!":
-			prioProp.Value = fmt.Sprint(vdir.PriorityLow)
-			t.Props.Add(prioProp)
-		default:
-			return fmt.Errorf("Unknown priority flag: %q, expected one of: !!!, !!, !", c.priority)
-		}
-	}
-
-	cal := ical.NewCalendar()
-	cal.Children = append(cal.Children, t)
 
 	p := path.Join(c.collection.Path, fmt.Sprintf("%s.ics", uid))
 
