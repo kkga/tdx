6a88478ee8456cca9490e69e1ccdd9a24863d662
Gadzhi Kharkharov <me@kkga.me>
2021-11-14 02:34:13 +0300

    read path env in root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,28 @@
 package cmd
 
 import (
-	_ "embed"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-//go:embed embed/help
-var helpTxt string
+// envPathVar is the environment variable for setting default vdir path
+const envPathVar = "TDX_PATH"
 
-var vdirPath string
+var (
+	// vdirPath is the path to user's vdir folder
+	vdirPath string
 
-var rootCmd = &cobra.Command{
-	Use:              "tdx",
-	Short:            "tdx -- todo manager for vdir (iCalendar) files",
-	Long:             `long output`,
-	Version:          "devdddd",
-	SilenceUsage:     true,
-	TraverseChildren: true,
-}
+	rootCmd = &cobra.Command{
+		Use:              "tdx",
+		Short:            "tdx -- todo manager for vdir (iCalendar) files",
+		Long:             `long output`,
+		Version:          "devdddd",
+		SilenceUsage:     true,
+		TraverseChildren: true,
+	}
+)
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
@@ -29,12 +31,15 @@ func Execute() {
 }
 
 func init() {
-	home, err := os.UserHomeDir()
-	cobra.CheckErr(err)
+	var defaultPath string
 
-	defaultPath := fmt.Sprintf("%s/.local/share/calendars/migadu", home)
+	if defaultPath = os.Getenv(envPathVar); defaultPath == "" {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		defaultPath = fmt.Sprintf("%s/.local/share/calendars", home)
+	}
 
-	rootCmd.Flags().StringVarP(&vdirPath, "path", "p", defaultPath, "path to vdir folder")
+	rootCmd.PersistentFlags().StringVarP(&vdirPath, "path", "p", defaultPath, "path to vdir folder")
 	rootCmd.MarkFlagRequired("path")
 
 	cobra.EnableCommandSorting = false
@@ -43,59 +48,11 @@ func init() {
 	rootCmd.AddCommand(NewDeleteCmd())
 }
 
-// func Root(args []string, version string) error {
-// 	if len(args) > 0 {
-// 		switch args[0] {
-// 		case "-h", "--help":
-// 			printHelp()
-// 			os.Exit(0)
-// 		case "-v", "--version":
-// 			fmt.Printf("tdx version %s\n", version)
-// 			os.Exit(0)
-// 		}
+// TODO: run default command if no subcommands
+// if len(args) == 0 {
+// 	defaultCmd := NewListCmd()
+// 	if err := defaultCmd.Init([]string{}); err != nil {
+// 		return err
 // 	}
-
-// 	cmds := []Runner{
-// 		NewListCmd(),
-// 		NewAddCmd(),
-// 		NewDoneCmd(),
-// 		NewShowCmd(),
-// 		NewDeleteCmd(),
-// 		NewEditCmd(),
-// 		NewPurgeCmd(),
-// 	}
-
-// 	if len(args) == 0 {
-// 		defaultCmd := NewListCmd()
-// 		if err := defaultCmd.Init([]string{}); err != nil {
-// 			return err
-// 		}
-// 		return defaultCmd.Run()
-// 	}
-
-// 	subcommand := os.Args[1]
-
-// 	for _, cmd := range cmds {
-// 		if cmd.Name() == subcommand || containsString(cmd.Alias(), subcommand) {
-// 			if err := cmd.Init(os.Args[2:]); err != nil {
-// 				return err
-// 			}
-// 			return cmd.Run()
-// 		}
-// 	}
-
-// 	return fmt.Errorf("Unknown subcommand: %q. See 'tdx -h'", subcommand)
-// }
-
-// func containsString(s []string, e string) bool {
-// 	for _, a := range s {
-// 		if a == e {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-// func printHelp() {
-// 	fmt.Print(helpTxt)
+// 	return defaultCmd.Run()
 // }
--- go.mod
+++ go.mod
@@ -19,5 +19,5 @@ require (
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 )
